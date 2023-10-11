35332ffca464356c73cc5179f0382a8ca52a017b
Chuc221 <bigbossa142k1nt@gmail.com>
2023-10-11 20:37:36 +0700

    layout login register

--- app/src/main/java/com/example/libraryapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/libraryapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.libraryapp
+package com.example.libraryapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.libraryapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
