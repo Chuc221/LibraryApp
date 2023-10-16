02e2b31d69572e42aca2e434f1b384605794c3ae
Chuc221 <bigbossa142k1nt@gmail.com>
2023-10-16 20:15:30 +0700

    layout profile, editprofile

--- app/src/main/java/com/example/libraryapp/ui/edit_profile/EditProfileFragment.kt
+++ app/src/main/java/com/example/libraryapp/ui/edit_profile/EditProfileFragment.kt
@@ -1,46 +1,50 @@
-package com.example.libraryapp.ui.home.user
+package com.example.libraryapp.ui.edit_profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.libraryapp.R
-import com.example.libraryapp.databinding.FragmentUserBinding
+import com.example.libraryapp.databinding.FragmentEditProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class UserFragment : Fragment() {
+class EditProfileFragment : Fragment() {
 
-    private lateinit var binding: FragmentUserBinding
+    private lateinit var binding: FragmentEditProfileBinding
     private lateinit var navigationController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_user, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_edit_profile, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        navigationController = Navigation.findNavController(view)
+        navigationController = findNavController()
         observeData()
-        setOnListener()
+        onClickListener()
     }
 
     private fun observeData() {
 
     }
 
-    private fun setOnListener() = with(binding){
-        imgBtnEdit.setOnClickListener {
-            navigationController.navigate(R.id.action_homeFragment_to_editProfileFragment)
+    private fun onClickListener() = with(binding){
+        back.setOnClickListener {
+            navigationController.popBackStack()
+        }
+
+        editProfile.setOnClickListener {
+            navigationController.popBackStack()
         }
     }
 
